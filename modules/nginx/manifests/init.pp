f2f796f05c2cc72462e9bf3bce119e4d75b6bc88
Geraint Palmer <geraintp@gmail.com>
2015-03-01 17:05:36 +0000

    make path and require package before configuration

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -17,6 +17,12 @@ class nginx {
 	file { '/etc/nginx/sites-enabled/default':
 		source => 'puppet:///modules/nginx/cat-pictures.conf',
 		notify => Service['nginx'],
+		require => Package['nginx'],
+	}
+	
+	file { [ "/var/www", "/var/www/cat-pictures" ]:
+	   ensure => directory,
+	   before => File ['/var/www/cat-pictures/index.html'],
 	}
 	
 	file { '/var/www/cat-pictures/index.html':
